37a182ef377c24c9d6daeca2eb16ba7a1f516737
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-08-16 13:05:35 +0200

    add GiftBar component

---
+++ src/components/GiftBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="gift-bar">
+        <div class="image-wrapper">
+			<img :src="src"/>
+		</div>
+
+		<div class="text-wrapper">
+			<div class="name">
+				{{ name }}
+			</div>
+
+			<div class="counter">
+				x{{ received }}
+			</div>
+		</div>
+    </div>
+</template>
+
+<script>
+export default {
+	props: {
+		item: {
+			type: Object,
+			required: true,
+		},
+	},
+
+	computed: {
+		src() {
+			return this.item.src;
+		},
+
+		name() {
+			return this.item.name;
+		},
+
+		received() {
+			return this.item.received;
+		},
+	},
+};
+</script>
