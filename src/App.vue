dbed3c08a5e4ab393bc1a71b0d294adbafe5779e
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-08-16 18:15:59 +0200

    add fetchTransactions logic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 	<div class="menu">
 		<input v-model="walletAddress" placeholder="your wallet address"/>
 
-		<button :disabled="isWalletAddressEmptyOrInvalid">Get summary</button>
+		<button :disabled="isWalletAddressEmptyOrInvalid || isLoading" @click="onSubmit">Get summary</button>
+
+		<div v-if="!!state" :style="{ color: 'red' }">
+			Current status: {{ state }}
+		</div>
 	</div>
 
 	<event-summary />
@@ -21,14 +25,79 @@ export default {
 		const httpService = useHttpServiceStore();
 
 		return {
+			error: null,
 			httpService,
 			walletAddress: "",
+			isLoading: false,
+			state: "",
+			claimGiftTransactions: [],
+
+			EVENT_START_TIMESTAMP_IN_SECONDS: 1722384000, // 31.07.24 00:00 GMT
+			MAX_PAGES_TO_FETCH_LIMIT: 10, // in case of some issue, prevent further fetch
+			GIFT_TRANSACTION_INPUT: "0x5ac66556", // when input starts with following data, it's claimGift
 		};
 	},
 
 	computed: {
 		isWalletAddressEmptyOrInvalid() {
 			return this.walletAddress.trim().length !== 42;
+		},
+	},
+
+	methods: {
+		async onSubmit() {
+			this.isLoading = true;
+
+			this.state = "Fetching transactions";
+
+			await this.fetchTransactions();
+
+			this.isLoading = false;
+
+			if (this.error) {
+				this.state = this.error;
+			} else {
+				this.state = "";
+			}
+		},
+
+		async fetchTransactions() {
+			let page = 1;
+			let isTransactionBeforeEventStartDate = false;
+
+			do {
+				try {
+					const transactions = await this.httpService.getTransactions(
+						this.walletAddress,
+						page,
+					);
+
+					for (const transaction of transactions) {
+						// if transaction is earlier than event start, end reached
+						if (transaction.blockTime < this.EVENT_START_TIMESTAMP_IN_SECONDS) {
+							isTransactionBeforeEventStartDate = true;
+
+							break;
+						}
+
+						if (transaction.input.startsWith(this.GIFT_TRANSACTION_INPUT)) {
+							this.claimGiftTransactions.push(transaction);
+						}
+					}
+				} catch (error) {
+					this.error = error;
+				}
+
+				page++;
+
+				if (
+					this.error ||
+					isTransactionBeforeEventStartDate ||
+					page > this.MAX_PAGES_TO_FETCH_LIMIT
+				) {
+					break;
+				}
+			} while (true);
 		},
 	},
 };
