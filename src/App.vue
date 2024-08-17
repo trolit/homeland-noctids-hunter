3eecaa2f8b17a571ebc5792646821755235ec37c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-08-17 12:01:02 +0200

    add fetch transactions logic

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,24 @@
 	<div class="menu">
 		<input v-model="walletAddress" placeholder="your wallet address"/>
 
-		<button :disabled="isWalletAddressEmptyOrInvalid || isLoading" @click="onSubmit">Get summary</button>
-
-		<div v-if="!!state" :style="{ color: 'red' }">
-			Current status: {{ state }}
-		</div>
+		<button 
+			:disabled="isWalletAddressEmptyOrInvalid || isLoading" 
+			@click="onSubmit"
+		>
+			Get summary
+		</button>
 	</div>
 
-	<event-summary :gifts="gifts" />
+	<event-summary :gifts="Object.values(gifts)" />
 </template>
 
 <script>
+import { debug } from "@/helpers/debug";
 import { sleep } from "@/helpers/sleep";
 import { ITransaction } from "@/types/ITransaction";
 import EventSummary from "@/components/EventSummary.vue";
+import { ITransactionDetails } from "./types/ITransactionDetails";
+import { secondsToCountdown } from "@/helpers/secondsToCountdown";
 import { useHttpServiceStore } from "@/store/useHttpServiceStore";
 import {
 	LOW_AXS_VALUE,
@@ -26,6 +30,7 @@ import {
 	MAX_PAGES_TO_FETCH_LIMIT,
 	GIFT_TRANSACTION_INPUT_START,
 	EVENT_START_TIMESTAMP_IN_SECONDS,
+	AAVE_PHANTOM_ALTAR_TOKEN_ID_START,
 } from "@/assets/constants";
 
 export default {
@@ -44,47 +49,47 @@ export default {
 			httpService,
 			walletAddress: "",
 			isLoading: false,
-			state: "",
 			claimGiftTransactions: [],
+			claimGiftTransactionsDetails: [],
 
-			gifts: [
-				{
+			gifts: {
+				lowSlp: {
 					tokenSymbol: SLP_TOKEN_SYMBOL,
 					value: "20",
 					received: 0,
 					src: slpSrc,
 				},
-				{
+				lowAxs: {
 					tokenSymbol: AXS_TOKEN_SYMBOL,
 					value: LOW_AXS_VALUE,
 					received: 0,
 					src: axsSrc,
 				},
-				{
+				highSlp: {
 					tokenSymbol: SLP_TOKEN_SYMBOL,
 					value: "100",
 					received: 0,
 					src: slpSrc,
 				},
-				{
+				highAxs: {
 					tokenSymbol: AXS_TOKEN_SYMBOL,
 					value: HIGH_AXS_VALUE,
 					received: 0,
 					src: axsSrc,
 				},
-				{
+				phantomAltar: {
 					tokenSymbol: ITEM_TOKEN_SYMBOL,
 					value: "Aave Phantom Altar",
 					received: 0,
 					src: require("@/assets/images/altar.png").default,
 				},
-				{
+				ronin: {
 					tokenSymbol: ITEM_TOKEN_SYMBOL,
 					value: "R0N1N-21",
 					received: 0,
 					src: require("@/assets/images/r0nin21.png").default,
 				},
-			],
+			},
 		};
 	},
 
@@ -98,17 +103,32 @@ export default {
 		async onSubmit() {
 			this.isLoading = true;
 
-			this.state = "Fetching transactions";
+			try {
+				debug("Collecting transactions...");
 
-			await this.fetchTransactions();
+				await this.fetchTransactions();
 
-			this.isLoading = false;
+				debug(
+					`Collecting transactions done! (${this.claimGiftTransactions.length})`,
+				);
 
-			if (this.error) {
-				this.state = this.error;
-			} else {
-				this.state = "";
+				debug("Collecting transactions details...");
+
+				await this.fetchTransactionsDetails();
+			} catch (error) {
+				this.error = error;
+
+				console.log(error);
+			} finally {
+				this.isLoading = false;
+
+				this.updateGifts();
+
+				debug("Total fetched transactions details ->");
+				debug(this.claimGiftTransactionsDetails);
 			}
+
+			debug("Everything completed!");
 		},
 
 		async fetchTransactions() {
@@ -116,41 +136,92 @@ export default {
 			let isTransactionBeforeEventStartDate = false;
 
 			do {
-				try {
-					const transactions = await this.httpService.getTransactions(
-						this.walletAddress,
-						page,
-					);
+				const transactions = await this.httpService.getTransactions(
+					this.walletAddress,
+					page,
+				);
 
-					for (const transaction of transactions) {
-						// if transaction is earlier than event start, end reached
-						if (transaction.blockTime < EVENT_START_TIMESTAMP_IN_SECONDS) {
-							isTransactionBeforeEventStartDate = true;
+				for (const transaction of transactions) {
+					// if transaction is earlier than event start, end reached
+					if (transaction.blockTime < EVENT_START_TIMESTAMP_IN_SECONDS) {
+						isTransactionBeforeEventStartDate = true;
 
-							break;
-						}
-
-						if (transaction.input.startsWith(GIFT_TRANSACTION_INPUT_START)) {
-							this.claimGiftTransactions.push(transaction);
-						}
+						break;
 					}
-				} catch (error) {
-					this.error = error;
+
+					if (transaction.input.startsWith(GIFT_TRANSACTION_INPUT_START)) {
+						this.claimGiftTransactions.push(transaction);
+					}
 				}
 
-				// sleep for 1 second between each requests to not overload API
-				await sleep(1);
+				// sleep between each requests to not overload network
+				await sleep(4);
 
 				page++;
 
 				if (
-					this.error ||
 					isTransactionBeforeEventStartDate ||
 					page > MAX_PAGES_TO_FETCH_LIMIT
 				) {
 					break;
 				}
 			} while (true);
+		},
+
+		async fetchTransactionsDetails() {
+			const DEBUG_STEP = 4;
+			const DELAY_BETWEEN_EACH_REQUEST_IN_SECONDS = 5;
+			const claimGiftTransactionsLength = 15;
+
+			for (let index = 0; index < claimGiftTransactionsLength; index++) {
+				const transactionDetails = await this.httpService.getTransactionDetails(
+					transaction.transactionHash,
+				);
+
+				const leftTransactions = claimGiftTransactionsLength - index - 1;
+
+				this.claimGiftTransactionsDetails.push(transactionDetails);
+
+				if (!leftTransactions) {
+					break;
+				}
+
+				await sleep(DELAY_BETWEEN_EACH_REQUEST_IN_SECONDS);
+
+				if (index >= DEBUG_STEP && index % DEBUG_STEP === 0) {
+					const delayInSeconds =
+						(leftTransactions - 1) * DELAY_BETWEEN_EACH_REQUEST_IN_SECONDS;
+
+					debug(
+						`Collecting transaction details... Estimated time: ${secondsToCountdown(delayInSeconds)}`,
+					);
+				}
+			}
+		},
+
+		updateGifts() {
+			debug("Updating summary UI...");
+
+			for (const { tokenSymbol, tokenId, value } of this
+				.claimGiftTransactionsDetails) {
+				if (tokenSymbol === ITEM_TOKEN_SYMBOL) {
+					tokenId.startsWith(AAVE_PHANTOM_ALTAR_TOKEN_ID_START)
+						? this.gifts.phantomAltar.received++
+						: this.gifts.ronin.received++;
+
+					continue;
+				}
+
+				if (tokenSymbol === AXS_TOKEN_SYMBOL) {
+					this.gifts[value === LOW_AXS_VALUE ? "lowAxs" : "highAxs"].received++;
+
+					continue;
+				}
+
+				if (tokenSymbol === SLP_TOKEN_SYMBOL) {
+					this.gifts[value === "20" ? "lowSlp" : "highSlp"].received++;
+				}
+			}
 		},
 	},
 };
